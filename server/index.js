524e27d0cae661db3cd55e3c10e3e7c145f6d688
Aastik Gupta <aastikgupta05@gmail.com>
2026-09-04 19:57:45 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -21,17 +21,13 @@ app.use("/api/cars", carRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/users", userroutes);
 
-const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("MongoDB connection error:", error.message);
   });
+
+module.exports = app;
